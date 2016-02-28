b9586a6e39f59359207d65f808c235d276448c81
fanghuang <fanghuang1992@gmail.com>
2016-02-28 07:50:33 -0600

    get data from monthly heatmap

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, request
+from datetime import datetime, date, timedelta
+# import calendar
 import json
-from datetime import datetime
 
 app = Flask(__name__)
 with open('daily.json', 'rb') as f:
@@ -11,12 +12,25 @@ lst = []
 for key, value in data.items():
 	lst.append([key.replace("-", ","), value])
 
-@app.route('/_add_numbers')
-def add_numbers():
+@app.route('/_get_data')
+def get_data():
 	"""Add two numbers server side, ridiculous but well..."""
-	a = request.args.get('a', 0, type=int)
-	b = request.args.get('b', 0, type=int)
-	return jsonify(result=a + b)
+	year_month = request.args.get('a', type=str)
+	year_month = year_month.split('-')
+	year = int(year_month[0])
+	month = int(year_month[1])
+	tmp = month
+	# my_cal = calendar.Calendar()
+	first = date(year, month, 1)
+	m_lst = []
+	while  month <  tmp + 1:
+		m_lst += [first]
+		first += timedelta(days=1)
+		month = first.month
+	# for i in my_cal().monthdatescalendar(int(my_cal[0]), int(my_cal[1])):
+	# 	m_lst += map(lambda x: str(x), i)
+	m_lst = sorted(map(lambda x: str(x), m_lst))
+	return jsonify(result=m_lst)
 
 
 @app.route('/')
@@ -27,9 +41,13 @@ def index():
 def calendar():
 	return render_template('calendar.html', lst = lst)
 
-@app.route('/daily')
-def daily():
-	return render_template('daily.html', lst = lst)
+@app.route('/heatmap')
+def heatmap():
+	return render_template('heatmap.html', lst = lst)
+
+@app.route('/prediction')
+def prediction():
+	return render_template('prediction.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
